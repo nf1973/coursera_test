9877729fb203e9eccf6006d12af31a4ff8444fd8
nf1973 <neil.fensom@gmail.com>
2021-11-17 00:30:50 +0100

    layout improvements

---
+++ playground/buttons/main.js
@@ -0,0 +1,7 @@
+function startGame() {
+    alert("Game cannot start yet!");
+}
+
+function btnClick(col) {
+    document.getElementById("result").innerHTML = col+" was clicked for some reason";
+  }
